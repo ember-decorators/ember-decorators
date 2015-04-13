e8c8b9b524f8dcbcd489d5da12bfeedd90b31069
Robert Jackson <robert.w.jackson@me.com>
2015-04-13 11:30:26 -0400

    Add initial implementation.

---
+++ addon/index.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+const { get, computed } = Ember;
+
+function handleDescriptor(target, key, descriptor) {
+  let originalParams = descriptor.__originalParams || [];
+  let computedDescriptor;
+
+  if (descriptor.writable) {
+    computedDescriptor = callUserSuppliedFunction(originalParams, descriptor.value);
+  } else {
+    throw new Error('ember-computed-decorators does not support using getters and setters');
+  }
+
+  descriptor.value = computed(originalParams, computedDescriptor);
+
+  return descriptor;
+}
+
+function callUserSuppliedFunction(params, func) {
+  return function() {
+    let paramValues = params.map(p => get(this, p));
+
+    return func.apply(this, paramValues);
+  };
+}
+
+function isDescriptor(item) {
+  return item && typeof item === 'object';
+}
+
+export default function computedDecorator(...params) {
+  // determine if user called as @computed('blah', 'blah') or @computed
+  if (isDescriptor(params[params.length - 1])) {
+    return handleDescriptor(...arguments);
+  } else {
+    return function(target, key, descriptor) {
+      descriptor.__originalParams = params;
+
+      return handleDescriptor(...arguments);
+    };
+  }
+}
---
+++ tests/unit/computed-test.js
@@ -0,0 +1,125 @@
+import Ember from "ember";
+import computed from "ember-computed-decorators";
+import { module, test, skip } from "qunit";
+
+const { get, set } = Ember;
+
+module('decorated computed with dependent keys', {
+  beforeEach() {
+
+  }
+});
+
+test('passes dependent keys into function as arguments', function(assert) {
+  var obj = {
+    first: 'rob',
+    last: 'jackson',
+
+    /* jshint ignore:start */
+    @computed('first', 'last')
+    /* jshint ignore:end */
+    name(first, last) {
+      assert.equal(first, 'rob');
+      assert.equal(last, 'jackson');
+    }
+  };
+
+  get(obj, 'name');
+});
+
+test('only calls getter when dependent keys change', function(assert) {
+  let callCount = 0;
+  let obj = {
+    first: 'rob',
+    last: 'jackson',
+
+    /* jshint ignore:start */
+    @computed('first', 'last')
+    /* jshint ignore:end */
+    name(first, last) {
+      callCount++;
+    }
+  };
+
+  get(obj, 'name');
+  assert.equal(callCount, 1);
+
+  get(obj, 'name');
+  assert.equal(callCount, 1);
+});
+
+test('throws an error when attempting to use ES6 getter/setter syntax', function(assert) {
+  assert.throws(function() {
+    let obj = {
+      first: 'rob',
+      last: 'jackson',
+
+      /* jshint ignore:start */
+      @computed('first', 'last')
+      /* jshint ignore:end */
+      set name(value) { },
+
+      get name() { }
+    };
+  });
+});
+
+skip('allows using ember-new-computed style get/set syntax', function(assert) {
+  // not currently supported by Babel (waiting on confirmation from @wycats
+  // before opening an issue)
+  let setCallCount = 0;
+  let getCallCount = 0;
+  let obj = {
+    first: 'rob',
+    last: 'jackson',
+
+    /* jshint ignore:start */
+    @computed('first', 'last')
+    /* jshint ignore:end */
+    name: {
+      get: function(first, last) {
+        assert.equal(first, 'rob');
+        assert.equal(last, 'jackson');
+
+        getCallCount++;
+      },
+
+      set: function(value, first, last) {
+        assert.equal(first, 'rob');
+        assert.equal(last, 'jackson');
+
+        setCallCount++;
+      }
+    }
+  };
+
+  get(obj, 'name');
+  assert.equal(getCallCount, 1, 'calls getter initially');
+
+  get(obj, 'name');
+  assert.equal(getCallCount, 1, 'does not call getter when cache is not busted');
+
+  set(obj, 'name', 'foo');
+  assert.equal(setCallCount, 1, 'calls setter when set');
+});
+
+module('decorated computed without dependent keys');
+
+test('works properly', function(assert) {
+  let callCount = 0;
+  let obj = {
+    first: 'rob',
+    last: 'jackson',
+
+    /* jshint ignore:start */
+    @computed
+    /* jshint ignore:end */
+    name() {
+      callCount++;
+    }
+  };
+
+  get(obj, 'name');
+
+  assert.equal(callCount, 1);
+});
