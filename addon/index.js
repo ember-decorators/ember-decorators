f8ec8d9a87e8ef007145abd149a60968303c6dd6
Stefan Penner <stefan.penner@gmail.com>
2015-04-14 20:18:28 -0700

    add other computed macros

--- addon/index.js
+++ addon/index.js
@@ -4,26 +4,31 @@ const { get } = Ember;
 
 import computed from 'ember-new-computed';
 
-function handleDescriptor(target, key, descriptor) {
-  let originalParams = descriptor.__originalParams || [];
-  let computedDescriptor;
+function handleDescriptor(target, key, desc, params = []) {
+  return {
+    enumerable: desc.enumerable,
+    configurable: desc.configurable,
+    writeable: desc.writeable,
+    initializer: function() {
+      let computedDescriptor;
 
-  if (descriptor.writable) {
-    if (typeof descriptor.value === 'object') {
-      let value = { };
-      if (descriptor.value.get) { value.get = callUserSuppliedGet(originalParams, descriptor.value.get); }
-      if (descriptor.value.set) { value.set = callUserSuppliedSet(originalParams, descriptor.value.set); }
-      computedDescriptor = value;
-    } else {
-      computedDescriptor = callUserSuppliedGet(originalParams, descriptor.value);
+      if (desc.writable) {
+        var val = desc.initializer();
+        if (typeof val === 'object') {
+          let value = { };
+          if (val.get) { value.get = callUserSuppliedGet(params, val.get); }
+          if (val.set) { value.set = callUserSuppliedSet(params, val.set); }
+          computedDescriptor = value;
+        } else {
+          computedDescriptor = callUserSuppliedGet(params, val);
+        }
+      } else {
+        throw new Error('ember-computed-decorators does not support using getters and setters');
+      }
+
+      return computed.apply(null, params.concat(computedDescriptor));
     }
-  } else {
-    throw new Error('ember-computed-decorators does not support using getters and setters');
-  }
-
-  descriptor.value = computed.apply(null, originalParams.concat(computedDescriptor));
-
-  return descriptor;
+  };
 }
 
 function callUserSuppliedGet(params, func) {
@@ -53,23 +58,21 @@ export default function computedDecorator(...params) {
   if (isDescriptor(params[params.length - 1])) {
     return handleDescriptor(...arguments);
   } else {
-    return function(target, key, descriptor) {
-      descriptor.__originalParams = params;
-
-      return handleDescriptor(...arguments);
+    return function(/* target, key, desc */) {
+      return handleDescriptor(...arguments, params);
     };
   }
 }
 
-export function readOnly(target, name, descriptor) {
-  descriptor.writable = false;
-  var value = typeof descriptor === 'object' && descriptor && descriptor.value;
-
-  if (value && typeof value === 'object' && value.isDescriptor) {
-    value.readOnly();
-  }
-
-  return descriptor;
+export function readOnly(target, name, desc) {
+  return {
+    writable:     false,
+    enumerable:   desc.enumerable,
+    configurable: desc.configurable,
+    initializer:  function() {
+      return desc.initializer().readOnly();
+    }
+  };
 }
 
 function decoratorAlias(fn, errorMessage) {
@@ -78,9 +81,15 @@ function decoratorAlias(fn, errorMessage) {
     if (params.length === 0) {
       throw new Error(errorMessage);
     } else {
-      return function(target, key, descriptor) {
-        descriptor.value = fn.apply(null, params.concat(descriptor.value));
-        return descriptor;
+      return function(target, key, desc) {
+        return {
+          enumerable: desc.enumerable,
+          configurable: desc.configurable,
+          writalbe: desc.writable,
+          initializer: function() {
+            return fn.apply(null, params.concat(desc.initializer()));
+          }
+        };
       };
     }
   };
@@ -88,3 +97,36 @@ function decoratorAlias(fn, errorMessage) {
 
 export var on = decoratorAlias(Ember.on, 'Can not `on` without event names');
 export var observes = decoratorAlias(Ember.observer, 'Can not `observe` without property names');
+
+function macroAlias(fn) {
+  return function(...params) {
+    return function(target, propertyName, desc) {
+      return {
+        enumerable: desc.enumerable,
+        configurable: desc.configurable,
+        writable: desc.writable,
+        initializer: function() {
+          return fn(...params);
+        }
+      };
+    };
+  };
+}
+
+export var alias = macroAlias(Ember.computed.alias);
+export var empty = macroAlias(Ember.computed.empty);
+export var notEmpty = macroAlias(Ember.computed.notEmpty);
+export var none = macroAlias(Ember.computed.none);
+export var not = macroAlias(Ember.computed.not);
+export var bool = macroAlias(Ember.computed.bool);
+export var match = macroAlias(Ember.computed.match);
+export var equal = macroAlias(Ember.computed.equal);
+export var gt = macroAlias(Ember.computed.gt);
+export var gte = macroAlias(Ember.computed.gte);
+export var lt = macroAlias(Ember.computed.lt);
+export var lte = macroAlias(Ember.computed.lte);
+export var and = macroAlias(Ember.computed.and);
+export var or = macroAlias(Ember.computed.or);
+export var any = macroAlias(Ember.computed.any);
+export var collect = macroAlias(Ember.computed.collect);
+export var oneWay = macroAlias(Ember.computed.oneWay);
