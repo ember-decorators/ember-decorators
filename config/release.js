012a41bec5c8d5002d62f71b8f9f1acd0aebf520
Robert Jackson <robert.w.jackson@me.com>
2015-07-13 09:44:45 -0400

    Ensure proper encoding of `npm publish` output.

--- config/release.js
+++ config/release.js
@@ -3,8 +3,7 @@ var execSync = require('child_process').execSync;
 module.exports = {
   // Publish the new release to NPM after a successful push
   afterPush: function() {
-    var output = execSync('npm publish');
-    console.log('********');
+    var output = execSync('npm publish', { encoding: 'utf8' });
     console.log(output);
   }
 };
