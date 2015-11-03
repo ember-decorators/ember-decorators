aeadd2c43c2b7459152ac6c1745061dfb86d0fd8
Marten Schilstra <mail@martenschilstra.nl>
2015-11-03 15:43:47 +0100

    Updated dependencies to work with Ember 2.X

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,20 @@
 module.exports = {
   scenarios: [
     {
+      name: 'ember-1.13.x',
+      dependencies: {
+        'ember': '1.13.10',
+        'ember-data': '2.0.0'
+      }
+    },
+    {
+      name: 'ember-2.0.x',
+      dependencies: {
+        'ember': '2.0.2',
+        'ember-data': '2.0.0'
+      }
+    },
+    {
       name: 'default',
       dependencies: { }
     },
