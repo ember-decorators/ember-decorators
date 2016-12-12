ca0784b70774ef7e89d24be27605b2e7a9f6af60
Kelly Selden <kellyselden@gmail.com>
2016-12-12 14:54:32 -0800

    use computed from ember-macro-helpers

--- addon/utils/handle-descriptor.js
+++ addon/utils/handle-descriptor.js
@@ -1,8 +1,5 @@
-import Ember from 'ember';
-import expandPropertyList from 'ember-macro-helpers/expand-property-list';
 import extractValue from './extract-value';
-
-const { computed, get } = Ember;
+import computed from 'ember-macro-helpers/computed';
 
 export default function handleDescriptor(target, key, desc, params = []) {
   return {
@@ -10,42 +7,12 @@ export default function handleDescriptor(target, key, desc, params = []) {
     configurable: desc.configurable,
     writeable: desc.writeable,
     initializer: function() {
-      let computedDescriptor;
-
-      if (desc.writable) {
-        var val = extractValue(desc);
-        if (typeof val === 'object') {
-          let value = { };
-          if (val.get) { value.get = callUserSuppliedGet(params, val.get); }
-          if (val.set) { value.set = callUserSuppliedSet(params, val.set); }
-          computedDescriptor = value;
-        } else {
-          computedDescriptor = callUserSuppliedGet(params, val);
-        }
-      } else {
+      if (!desc.writable) {
         throw new Error('ember-computed-decorators does not support using getters and setters');
       }
 
-      return computed.apply(null, params.concat(computedDescriptor));
+      let value = extractValue(desc);
+      return computed(...params.concat(value));
     }
-  };
-}
-
-function callUserSuppliedGet(params, func) {
-  const expandedParams = expandPropertyList(params);
-  return function() {
-    let paramValues = expandedParams.map(p => get(this, p));
-
-    return func.apply(this, paramValues);
-  };
-}
-
-function callUserSuppliedSet(params, func) {
-  const expandedParams = expandPropertyList(params);
-  return function(key, value) {
-    let paramValues = expandedParams.map(p => get(this, p));
-    paramValues.unshift(value);
-
-    return func.apply(this, paramValues);
   };
 }
