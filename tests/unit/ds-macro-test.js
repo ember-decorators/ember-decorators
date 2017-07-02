473b37b02b209c1e106e430bfe167b0264e67c06
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-07-02 16:54:59 -0400

    maybe failing test?

--- tests/unit/ds-macro-test.js
+++ tests/unit/ds-macro-test.js
@@ -37,3 +37,32 @@ test('DS macro', function(assert) {
 
   assert.deepEqual(relationships, ['user', 'car']);
 });
+
+class ES6Model extends DS.Model {
+  @attr firstName;
+  @attr({ defaultTo: 'blue' }) lastName;
+  @hasMany user;
+  @belongsTo car;
+}
+
+test('DS macro (ES6)', function(assert) {
+  ES6Model.store = {
+    modelFor(typeKey) {
+      return typeKey;
+    }
+  };
+
+  var attributes = [];
+  ES6Model.eachAttribute(function(attr) {
+    attributes.push(attr);
+  });
+
+  assert.deepEqual(attributes, ['firstName', 'lastName']);
+
+  var relationships = [];
+  ES6Model.eachRelationship(function(attr) {
+    relationships.push(attr);
+  });
+
+  assert.deepEqual(relationships, ['user', 'car']);
+});
