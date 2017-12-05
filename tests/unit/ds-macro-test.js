10d74ed398875aa0b5d1b3d42f5a544a55b242e3
snewcomer24 <snewcomer24@gmail.com>
2017-12-05 09:40:37 -0800

    Ember Data ES6 macro fix

--- tests/unit/ds-macro-test.js
+++ tests/unit/ds-macro-test.js
@@ -1,4 +1,4 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
 import {
   attr,
   hasMany,
@@ -10,7 +10,7 @@ import { module, test } from 'qunit';
 module('ember-data macro decorator');
 
 test('DS macro', function(assert) {
-  var Model = DS.Model.extend({
+  let ObjectModel = Model.extend({
     @attr firstName: null,
     @attr({ defaultTo: 'blue' }) lastName: null,
     @attr('number') age: null,
@@ -18,14 +18,14 @@ test('DS macro', function(assert) {
     @belongsTo car: null
   });
 
-  Model.store = {
+  ObjectModel.store = {
     modelFor(typeKey) {
       return typeKey;
     }
   };
 
-  var attributes = [];
-  Model.eachAttribute(function(attr, meta) {
+  let attributes = [];
+  ObjectModel.eachAttribute(function(attr, meta) {
     attributes.push({name: attr, type: meta.type});
   });
 
@@ -35,10 +35,47 @@ test('DS macro', function(assert) {
     {name: 'age',       type: 'number'}
   ]);
 
-  var relationships = [];
-  Model.eachRelationship(function(attr) {
+  let relationships = [];
+  ObjectModel.eachRelationship(function(attr) {
     relationships.push(attr);
   });
 
   assert.deepEqual(relationships, ['user', 'car']);
+});
+
+test('DS macro with ES6 class', function(assert) {
+  class ES6 extends Model {
+    @attr firstName;
+    @attr({ defaultTo: 'blue' }) lastName;
+    @attr('number') age;
+    @hasMany('user') users;
+    @belongsTo car;
+  }
+
+  ES6.store = {
+    modelFor(typeKey) {
+      return typeKey;
+    }
+  };
+
+  let attributes = [];
+  ES6.eachAttribute(function(attr, meta) {
+    attributes.push({name: attr, type: meta.type});
+  });
+
+  assert.deepEqual(attributes, [
+    {name: 'firstName', type: undefined},
+    {name: 'lastName',  type: undefined},
+    {name: 'age',       type: 'number'}
+  ]);
+
+  let relationships = [];
+  ES6.eachRelationship(function(attr, meta) {
+    relationships.push({key: attr, type: meta.type});
+  });
+
+  assert.deepEqual(relationships, [
+    {key: 'users', type: 'user'},
+    {key: 'car', type: 'car'}
+  ]);
 });
