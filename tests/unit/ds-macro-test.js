f7f0e00af28277f47a37b23a02b5eead238cf8c7
snewcomer24 <snewcomer24@gmail.com>
2017-12-06 08:07:46 -0800

    add test with both sides

--- tests/unit/ds-macro-test.js
+++ tests/unit/ds-macro-test.js
@@ -79,3 +79,57 @@ test('DS macro with ES6 class', function(assert) {
     {key: 'car', type: 'car'}
   ]);
 });
+
+test('DS macro with multiple ES6 classes', function(assert) {
+  class Storefront extends Model {
+    @attr address;
+    @hasMany('user') users;
+    @belongsTo car;
+  }
+
+  class User extends Model {
+    @attr name;
+    @attr('number') age;
+    @hasMany('storefront') storefronts;
+  }
+
+  // Storefront
+  let attributes = [];
+  Storefront.eachAttribute(function(attr, meta) {
+    attributes.push({name: attr, type: meta.type});
+  });
+
+  assert.deepEqual(attributes, [
+    {name: 'address', type: undefined},
+  ]);
+
+  let relationships = [];
+  Storefront.eachRelationship(function(attr, meta) {
+    relationships.push({key: attr, type: meta.type});
+  });
+
+  assert.deepEqual(relationships, [
+    {key: 'users', type: 'user'},
+    {key: 'car', type: 'car'}
+  ]);
+
+  // User
+  let user_attributes = [];
+  User.eachAttribute(function(attr, meta) {
+    user_attributes.push({name: attr, type: meta.type});
+  });
+
+  assert.deepEqual(user_attributes, [
+    {name: 'name', type: undefined},
+    {name: 'age',  type: 'number'}
+  ]);
+
+  let user_relationships = [];
+  User.eachRelationship(function(attr, meta) {
+    user_relationships.push({key: attr, type: meta.type});
+  });
+
+  assert.deepEqual(user_relationships, [
+    {key: 'storefronts', type: 'storefront'}
+  ]);
+});
