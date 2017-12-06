21c7c777b9f1e3300bd89281042237985566e5ad
Jan Buschtöns <jan@buschtoens.me>
2017-12-06 08:50:59 +0100

    fix(build): shim `emberDecorators.disableTransforms` setting
    Addresses https://github.com/ember-decorators/ember-decorators/pull/182#issuecomment-347255423

--- index.js
+++ index.js
@@ -4,5 +4,16 @@
 const BabelTransforms = require('@ember-decorators/babel-transforms');
 
 module.exports = Object.assign({}, BabelTransforms, {
-  name: 'ember-decorators'
+  name: 'ember-decorators',
+
+  included(parent) {
+    // shim support for `emberDecorators.disableTransforms` and forward the
+    // setting to `@ember-decorators/babel-transforms.disable`
+    if (parent.options && parent.options.emberDecorators && parent.options.emberDecorators.disableTransforms) {
+      parent.options['@ember-decorators/babel-transforms'] = parent.options['@ember-decorators/babel-transforms'] || {};
+      parent.options['@ember-decorators/babel-transforms'].disable = true;
+    }
+
+    BabelTransforms.included.apply(this, arguments);
+  }
 });
