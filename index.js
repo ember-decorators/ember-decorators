e571f1c656e25829316f0e6c03daa52fe3109e50
Jan Buschtöns <jan@buschtoens.me>
2017-12-14 08:05:46 +0100

    refactor(build): call `setupBabel` helper instead of brittle fake inheritance
    Faking inheritance in order to let `@ember-decorators/babel-transforms` handle the babel setup logic was brittle and was incompatible with calling `this._super.included` inside the `babel-transforms` addon.
    
    The following commit extracts the babel setup logic into a separate module, that may be imported for easy consumption.
    
    https://github.com/ember-decorators/babel-transforms/pull/5/commits/b0ec8d557d06dc6604ed9dc0449127597263de8d

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /* eslint-env node */
 'use strict';
 
-const BabelTransforms = require('@ember-decorators/babel-transforms');
+const setupBabel = require('@ember-decorators/babel-transforms/setup-babel');
 
-module.exports = Object.assign({}, BabelTransforms, {
+module.exports = {
   name: 'ember-decorators',
 
   included(parent) {
+    this._super.included.apply(this, arguments);
+
     // shim support for `emberDecorators.disableTransforms` and forward the
     // setting to `@ember-decorators/babel-transforms.disable`
     if (parent.options && parent.options.emberDecorators && parent.options.emberDecorators.disableTransforms) {
@@ -14,6 +16,13 @@ module.exports = Object.assign({}, BabelTransforms, {
       parent.options['@ember-decorators/babel-transforms'].disable = true;
     }
 
-    BabelTransforms.included.apply(this, arguments);
+    const isDisabled = parent.options &&
+      parent.options['@ember-decorators/babel-transforms'] &&
+      parent.options['@ember-decorators/babel-transforms'].disable;
+
+    if (!this._registeredWithBabel && !isDisabled) {
+      setupBabel(parent);
+      this._registeredWithBabel = true;
+    }
   }
-});
+};
