a35a3a2a0583e80ee0d38dc2e0579fa5d641a305
Jan Buschtöns <buschtoens@gmail.com>
2018-03-07 20:47:43 +0100

    fix(installation): [ember-cli < 2.16] prompt user
    ...to manually install @ember-decorators/babel-transforms instead of crashing.

--- blueprints/ember-decorators/index.js
+++ blueprints/ember-decorators/index.js
@@ -1,13 +1,35 @@
 /* eslint-env node */
+const semver = require('semver');
+
 module.exports = {
   description: 'Adds @ember-decorators/babel-transforms to the parent app',
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall() {
-    return this.addAddonToProject({
-      name: '@ember-decorators/babel-transforms',
-      target: '^2.0.0'
-    });
+    const name = '@ember-decorators/babel-transforms';
+    const target = '^2.0.0';
+
+    const emberCLIVersion = this.project.emberCLIVersion();
+    const minEmberCLIVersion = '2.16.0';
+
+    if (semver.lt(emberCLIVersion, minEmberCLIVersion)) {
+      // Installing an addon with a scoped packages name is only supported since ember-cli 2.16.0-beta.1
+      // https://github.com/ember-cli/ember-cli/commit/a69016db62bdae2ac76acb1b98c9f789223cf083
+      this.project.ui.writeWarnLine(
+        'Could not automatically install the required dependency ' + name +
+        '. You are using an outdated version (v' + emberCLIVersion + ') of ember-cli.'
+      );
+      this.project.ui.writeInfoLine(
+        'You can fix this by either updating ember-cli to at least v' +
+        minEmberCLIVersion + ' or manually installing ' + name + ', like so:'
+      );
+      this.project.ui.writeInfoLine('');
+      this.project.ui.writeInfoLine('  For npm users:   npm install -D ' + name + '@' + target);
+      this.project.ui.writeInfoLine('  For yarn users:  yarn add -D ' + name + '@' + target);
+      this.project.ui.writeInfoLine('');
+    } else {
+      return this.addAddonToProject({ name, target });
+    }
   }
 };
