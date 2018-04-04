2686bcce705aa4b30cf069e18f592647d6d0b95d
allthesignals <wilburnforce@gmail.com>
2018-04-04 12:23:53 -0400

    use format for MU template

--- blueprints/component/module-unification-files/__root__/__path__/component.js
+++ blueprints/component/module-unification-files/__root__/__path__/component.js
@@ -1,4 +1,4 @@
 import Component from '@ember/component';
 
-export default class <%= camelizedModuleName %> extends Component {
+export default class <%= lastPathSegment %>Component extends Component {
 }
