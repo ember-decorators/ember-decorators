642bf48f4a3f901eb1cc0613f9e7250e143151d9
Iaroslav Popov <chilicoder@users.noreply.github.com>
2018-05-09 22:45:18 +0200

    tests added to controller generator

---
+++ packages/-ember-decorators/node-tests/blueprints/controller-test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const EOL = require('os').EOL;
+const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+const setupTestHooks = blueprintHelpers.setupTestHooks;
+const emberNew = blueprintHelpers.emberNew;
+const emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+
+const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
+
+describe('Acceptance: ember generate and destroy controller', function() {
+  setupTestHooks(this);
+
+  describe('in app', function() {
+    beforeEach(function() {
+      return emberNew();
+    });
+
+    it('controller foo', function() {
+      return emberGenerateDestroy(['controller', 'foo'], _file => {
+        expect(_file('app/controllers/foo.js'))
+          .to.contain("import Controller from '@ember/controller';")
+          .to.contain('export default class FooController extends Controller {\n}');
+
+        expect(_file('tests/unit/controllers/foo-test.js'))
+          .to.exist;
+      });
+    });
+
+    it('controller foo/bar', function() {
+      return emberGenerateDestroy(['controller', 'foo/bar'], _file => {
+        expect(_file('app/controllers/foo/bar.js'))
+          .to.contain("import Controller from '@ember/controller';")
+          .to.contain('export default class FooBarController extends Controller {\n}');
+
+        expect(_file('tests/unit/controllers/foo/bar-test.js'))
+          .to.exist;
+      });
+    });
+
+  });
+});
