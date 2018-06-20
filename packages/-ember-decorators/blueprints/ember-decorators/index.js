29e54bed00ac7198c2f02d63ef0abb80a9ffca66
Mike Behnke <mike.behnke@joolhealth.com>
2018-06-20 17:14:21 -0400

    Update afterInstall to only mod .eslintrc if it doesn't already have a parser installed
    - also update tsconfig.json file if it exists to add experimentalDecorators=true
    
    Fixes #244
    Fixes #245

--- packages/-ember-decorators/blueprints/ember-decorators/index.js
+++ packages/-ember-decorators/blueprints/ember-decorators/index.js
@@ -34,26 +34,39 @@ module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall() {
+    // load .eslintrc
     let eslintrcPath = path.join(this.project.root, '.eslintrc.js');
-    let eslintrc = fs.readFileSync(eslintrcPath, { encoding: 'utf8' });
-    let lines = eslintrc.split('\n');
+    let eslintrc = fs.existsSync(eslintrcPath) && fs.readFileSync(eslintrcPath, { encoding: 'utf8' });
+    let eslintrcLines = eslintrc && eslintrc.split('\n');
+    // load tsconfig.json
+    let tsconfigPath = path.join(this.project.root, 'tsconfig.json');
+    let tsconfigJSON = fs.existsSync(tsconfigPath) && fs.readFileSync(tsconfigPath, { encoding: 'utf8' });
+    let tsconfig = tsconfigJSON && JSON.parse(tsconfigJSON);
 
-    let parserIndex = lines.findIndex(line => line.match(/parser:/));
-    let rootIndex = lines.findIndex(line => line.match(/root:/));
+    // Mod .eslintrc file to add parser if it doesn't already have one defined
+    let parserIndex = eslintrcLines && eslintrcLines.findIndex(line => line.match(/parser:/));
+    let rootIndex = eslintrcLines && eslintrcLines.findIndex(line => line.match(/root:/));
 
     if (parserIndex > -1) {
-      lines.splice(parserIndex, 1);
-    }
-
-    if (rootIndex > -1) {
-      lines.splice(rootIndex + 1, 0, `  parser: 'babel-eslint',`);
+      this.project.ui.writeWarnLine(
+        `Found an ESLint parser already defined, and so could not automatically set the eslint parser. You 
+         may need to add \`parser: 'babel-eslint'\` to your eslintrc.js. Note: if specifically using another 
+         parser (like, \`parser: 'typescript-eslint-parser'\`), this may not be necessary.`
+      );
+    } else if (eslintrcLines && rootIndex > -1) {
+      eslintrcLines.splice(rootIndex + 1, 0, `  parser: 'babel-eslint',`);
+      fs.writeFileSync(eslintrcPath, eslintrcLines.join('\n'));
     } else {
       this.project.ui.writeWarnLine(
         'Could not automatically set the eslint parser, add `parser: \'babel-eslint\' to your eslintrc.js'
       );
     }
 
-    fs.writeFileSync(eslintrcPath, lines.join('\n'));
+    // If tsconfig file found, add experimentalDecorators support
+    if (tsconfig && typeof tsconfig.compilerOptions === 'object' && !tsconfig.compilerOptions.experimentalDecorators) {
+      tsconfig.compilerOptions.experimentalDecorators = true;
+      fs.writeFileSync(tsconfigPath, JSON.stringify(tsconfig, null, 2));
+    }
 
     return Promise.all([
       addScopedAddonToProject(this, {
