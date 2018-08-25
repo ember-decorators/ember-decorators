ddc0bfaa97181589bdec4607a202b86480622141
Richard Viney <richard@lightspeedgraphics.co.nz>
2018-08-25 15:49:13 +1200

    Add types for @observes, @unobserves, @on, and @off

--- packages/object/index.d.ts
+++ packages/object/index.d.ts
@@ -69,6 +69,90 @@ export function computed(
 ): PropertyDescriptor;
 
 /**
+  Triggers the target function when the dependent properties have changed
+
+  ```javascript
+  import { observes } from '@ember-decorators/object';
+
+  class Foo {
+    @observes('foo')
+    bar() {
+      //...
+    }
+  }
+  ```
+
+  @function
+  @param {...String} propertyNames - Names of the properties that trigger the function
+ */
+export function observes(...propertyNames: string[]): MethodDecorator;
+
+/**
+  Removes observers from the target function.
+
+  ```javascript
+  import { observes, unobserves } from '@ember-decorators/object';
+
+  class Foo {
+    @observes('foo')
+    bar() {
+      //...
+    }
+  }
+
+  class Bar extends Foo {
+    @unobserves('foo') bar;
+  }
+  ```
+
+  @function
+  @param {...String} propertyNames - Names of the properties that no longer trigger the function
+ */
+export function unobserves(...propertyNames: string[]): PropertyDecorator;
+
+/**
+  Adds an event listener to the target function.
+
+  ```javascript
+  import { on } from '@ember-decorators/object';
+
+  class Foo {
+    @on('fooEvent', 'barEvent')
+    bar() {
+      //...
+    }
+  }
+  ```
+
+  @function
+  @param {...String} eventNames - Names of the events that trigger the function
+ */
+export function on(...eventNames: string[]): MethodDecorator;
+
+/**
+  Removes an event listener from the target function.
+
+  ```javascript
+  import { on, off } from '@ember-decorators/object';
+
+  class Foo {
+    @on('fooEvent', 'barEvent')
+    bar() {
+      //...
+    }
+  }
+
+  class Bar extends Foo {
+    @off('fooEvent', 'barEvent') bar;
+  }
+  ```
+
+  @function
+  @param {...String} eventNames - Names of the events that no longer trigger the function
+ */
+export function off(...eventNames: string[]): PropertyDecorator;
+
+/**
  * Decorator that modifies a computed property to be read only.
  *
  * Usage:
