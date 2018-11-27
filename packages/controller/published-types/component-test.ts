30d75d9314893247c6bc6e4d9c01f8d72b686fed
Mike North <michael.l.north@gmail.com>
2018-11-27 23:06:45 +0800

    chore: CI testing of type declarations

--- packages/controller/published-types/component-test.ts
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-import { tagName } from '@ember-decorators/component';
-
-@tagName('span')
-export class XFoo extends Component {}
-
-// $ExpectError
-@tagName
-export class XFoo2 extends Component {}
--- packages/controller/published-types/index.d.ts
+++ packages/controller/published-types/index.d.ts
@@ -1,238 +1,62 @@
-// TypeScript Version: 2.1
+// TypeScript Version: 2.7
 
-import { TemplateFactory } from 'htmlbars-inline-precompile';
+import { Registry } from '@ember/controller';
 
 /**
- * Decorator which indicates that the field or computed should be bound
- * to an attribute value on the component. This replaces `attributeBindings`
- * by directly allowing you to specify which properties should be bound.
+ * Decorator that wraps `Ember.inject.controller`
  *
- * ```js
- * import Component from '@ember/component';
- * import { attribute } from 'ember-decorators/component';
- * import { computed } from 'ember-decorators/object';
+ * Injects a controller into a Controller as the decorated property
  *
- * export default class AttributeDemoComponent extends Component {
- *   @attribute role = 'button';
+ *  ```javascript
+ * import Controller from '@ember/controller';
+ * import { controller } from 'ember-decorators/controller';
  *
- *   // With provided attribute name
- *   @attribute('data-foo') foo = 'lol';
- *
- *   @attribute
- *   @computed
- *   get id() {
- *     // return generated id
- *   }
+ * export default class IndexController extends Controller {
+ *   @controller() application;
  * }
  * ```
  *
  * @function
+ * @param {String} [controllerName] - The name of the controller to inject. If not provided, the property name will be used
  */
-export function attribute(name?: string): PropertyDecorator;
+export function controller(target: any, key: any): any;
 /**
- * Decorator which indicates that the field or computed should be bound
- * to an attribute value on the component. This replaces `attributeBindings`
- * by directly allowing you to specify which properties should be bound.
+ * Decorator that wraps `Ember.inject.controller`
  *
- * ```js
- * import Component from '@ember/component';
- * import { attribute } from 'ember-decorators/component';
- * import { computed } from 'ember-decorators/object';
+ * Injects a controller into a Controller as the decorated property
  *
- * export default class AttributeDemoComponent extends Component {
- *   @attribute role = 'button';
+ *  ```javascript
+ * import Controller from '@ember/controller';
+ * import { controller } from 'ember-decorators/controller';
  *
- *   // With provided attribute name
- *   @attribute('data-foo') foo = 'lol';
- *
- *   @attribute
- *   @computed
- *   get id() {
- *     // return generated id
- *   }
+ * export default class IndexController extends Controller {
+ *   @controller() application;
  * }
  * ```
  *
  * @function
+ * @param {String} [controllerName] - The name of the controller to inject. If not provided, the property name will be used
  */
-export function attribute(
+export function controller(
   target: any,
   key: any,
   descriptor: PropertyDescriptor
 ): PropertyDescriptor;
 /**
- * Decorator which indicates that the field or computed should be bound
- * to an attribute value on the component. This replaces `attributeBindings`
- * by directly allowing you to specify which properties should be bound.
+ * Decorator that wraps `Ember.inject.controller`
  *
- * ```js
- * import Component from '@ember/component';
- * import { attribute } from 'ember-decorators/component';
- * import { computed } from 'ember-decorators/object';
+ * Injects a controller into a Controller as the decorated property
  *
- * export default class AttributeDemoComponent extends Component {
- *   @attribute role = 'button';
+ *  ```javascript
+ * import Controller from '@ember/controller';
+ * import { controller } from 'ember-decorators/controller';
  *
- *   // With provided attribute name
- *   @attribute('data-foo') foo = 'lol';
- *
- *   @attribute
- *   @computed
- *   get id() {
- *     // return generated id
- *   }
+ * export default class IndexController extends Controller {
+ *   @controller() application;
  * }
  * ```
  *
  * @function
+ * @param {String} [controllerName] - The name of the controller to inject. If not provided, the property name will be used
  */
-export function attribute(target: any, key: any): any;
-
-/**
- * Decorator which indicates that the field or computed should be bound to
- * the component class names. This replaces `classNameBindings` by directly
- * allowing you to specify which properties should be bound.
- *
- * ```js
- * import Component from '@ember/component';
- * import { className } from 'ember-decorators/component';
- * import { computed } from 'ember-decorators/object';
- *
- * export default class ClassNameDemoComponent extends Component {
- *   @className boundField = 'default-class';
- *
- *   // With provided true/false class names
- *   @className('active', 'inactive') isActive = true;
- *
- *   @className
- *   @computed
- *   get boundComputed() {
- *     // return generated class
- *   }
- * }
- * ```
- *
- * @function
- */
-export function className(target: any, key: any): any;
-/**
- * Decorator which indicates that the field or computed should be bound to
- * the component class names. This replaces `classNameBindings` by directly
- * allowing you to specify which properties should be bound.
- *
- * ```js
- * import Component from '@ember/component';
- * import { className } from 'ember-decorators/component';
- * import { computed } from 'ember-decorators/object';
- *
- * export default class ClassNameDemoComponent extends Component {
- *   @className boundField = 'default-class';
- *
- *   // With provided true/false class names
- *   @className('active', 'inactive') isActive = true;
- *
- *   @className
- *   @computed
- *   get boundComputed() {
- *     // return generated class
- *   }
- * }
- * ```
- *
- * @function
- */
-export function className(
-  target: any,
-  key: any,
-  descriptor: PropertyDescriptor
-): PropertyDescriptor;
-/**
- * Decorator which indicates that the field or computed should be bound to
- * the component class names. This replaces `classNameBindings` by directly
- * allowing you to specify which properties should be bound.
- *
- * ```js
- * import Component from '@ember/component';
- * import { className } from 'ember-decorators/component';
- * import { computed } from 'ember-decorators/object';
- *
- * export default class ClassNameDemoComponent extends Component {
- *   @className boundField = 'default-class';
- *
- *   // With provided true/false class names
- *   @className('active', 'inactive') isActive = true;
- *
- *   @className
- *   @computed
- *   get boundComputed() {
- *     // return generated class
- *   }
- * }
- * ```
- *
- * @function
- */
-export function className(
-  trueValue?: string,
-  falseValue?: string
-): PropertyDecorator;
-
-/**
- * Class decorator which specifies the class names to be applied to a component.
- * This replaces the `classNames` property on components in the traditional Ember
- * object model.
- *
- * ```js
- * import Component from '@ember/component';
- * import { classNames } from 'ember-decorators/component';
- *
- * @classNames('a-static-class', 'another-static-class')
- * export default class ClassNamesDemoComponent extends Component {}
- * ```
- *
- * @param {...String} classNames - The list of classes to be applied to the component
- */
-export function classNames(...classNames: string[]): ClassDecorator;
-
-/**
- * Class decorator which specifies the tag name of the component. This replaces
- * the `tagName` property on components in the traditional Ember object model.
- *
- * ```js
- * import Component from '@ember/component';
- * import { tagName } from 'ember-decorators/component';
- *
- * @tagName('button')
- * export default class TagNameDemoComponent extends Component {}
- * ```
- *
- * @param {String} tagName - The HTML tag to be used for the component
- */
-export function tagName(tagName: string): ClassDecorator;
-
-/**
-  Class decorator which specifies the layout for the component. This replaces
-  the `layout` property on components in the traditional Ember object model.
-
-  ```js
-  import template from '../templates/components/x-foo';
-
-  @layout(template)
-  export default class TagNameDemoComponent extends Component {}
-  ```
-
-  ```js
-  import hbs from 'htmlbars-inline-precompile';
-
-  @layout(hbs`<h1>Hello {{ name }}</h1>`)
-  export default class TagNameDemoComponent extends Component {
-    constructor() {
-      super(...arguments);
-      this.set('name', 'Tomster');
-    }
-  }
-  ```
-
-  @param {TemplateFactory} template - The compiled template to be used for the component
-*/
-export function layout(template: TemplateFactory): ClassDecorator;
+export function controller(name: keyof Registry): PropertyDecorator;
