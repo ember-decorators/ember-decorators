5f6b568d26f248b3e5909628012b3d4b8b662fa0
Chris Garrett <me@pzuraq.com>
2018-12-01 13:19:28 -0800

    [BUGFIX] Checks devDeps for ec-typescript (#324)
    `ember-cli-typescript` v1 would precompile addons _before_ they were
    published, meaning it would be in `devDependencies` and
    ember-cli-version-checker would not be able to pick up on them. This
    adds a check to dependencies/devDependencies for `ember-cli-typescript`,
    which should solve a lot of the larger ecosystem bugs that have been
    popping up.

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs-extra');
+const path = require('path');
+const semver = require('semver');
 const VersionChecker = require('ember-cli-version-checker');
 
 const PLUGIN_NAME = 'ember-decorators-debug-macros';
@@ -5,6 +8,23 @@ const PLUGIN_NAME = 'ember-decorators-debug-macros';
 // used as a way to memoize needsStage1Decorators so that we can avoid many many
 // top down searches of the addon heirarchy
 const NEEDS_STAGE_1_DECORATORS = new WeakMap();
+
+function cleanVersion(version) {
+  return version.match(/\d\.\d\.\d/)[0];
+}
+
+function getDependenciesFor(root) {
+  try {
+    let { dependencies = {}, devDependencies } = fs.readJsonSync(path.join(root, 'package.json'));
+
+    // we want the union of dependencies and dev deps
+    Object.assign(dependencies, devDependencies);
+
+    return dependencies;
+  } catch (e) {
+    return false;
+  }
+}
 
 function checkNeedsStage1Decorators(project) {
   if (NEEDS_STAGE_1_DECORATORS.has(project)) {
@@ -19,16 +39,40 @@ function checkNeedsStage1Decorators(project) {
 }
 
 function checkAddonsForStage1(project, addonOrProject) {
-  if (!addonOrProject.addons || !addonOrProject.addons.length > 0) return false;
+  let dependencies = getDependenciesFor(addonOrProject.root);
+
+  if (!dependencies) {
+    // for some reason we couldn't read package.json, return true to be safe
+    project.ui.writeWarnLine(`unable to read package.json for ${addonOrProject.name}, including stage 1 decorators by default`);
+
+    return true;
+  }
 
   let checker = new VersionChecker(addonOrProject);
 
-  let decoratorChecker = checker.for('@ember-decorators/utils', 'npm');
-  let hasDecorators = decoratorChecker.exists();
-  let hasOlderDecorators = decoratorChecker.lt('3.1.0');
+  // check if decorators are in deps at all. We must check directly because it's
+  // possible to "bubble" up to the main app's decorators, and we may give a
+  // false positive.
+  let hasDecorators = [
+    'ember-decorators',
+    '@ember-decorators/component',
+    '@ember-decorators/controller',
+    '@ember-decorators/data',
+    '@ember-decorators/object',
+    '@ember-decorators/service',
+    '@ember-decorators/utils',
+  ].some(d => d in dependencies);
 
-  let hasOlderTypescript = checker.for('ember-cli-typescript', 'npm').lt('2.0.0-beta.1');
+  // check the resolved version of decorators. This could end up being the root
+  // application's version of decorators, so we need to use ec-version-checker.
+  let hasOlderDecorators = checker.for('@ember-decorators/utils', 'npm').lt('3.1.0');
+
+  // If embroiderer comes through this will need to update.
   let hasOlderTransforms = checker.for('@ember-decorators/babel-transforms', 'npm').lt('3.1.0');
+
+  let hasOlderTypescript =
+    'ember-cli-typescript' in dependencies &&
+    semver.lt(cleanVersion(dependencies['ember-cli-typescript']), '2.0.0-beta.1');
 
   let needsStage1 = false;
 
@@ -53,7 +97,7 @@ function checkAddonsForStage1(project, addonOrProject) {
     needsStage1 = true;
   }
 
-  if (hasOlderDecorators) {
+  if (hasDecorators && hasOlderDecorators) {
     project.ui.writeWarnLine(
       `${addonOrProjectMessage} is using an older version of ember-decorators that uses stage 1 decorators. Please update to ember-decorators@v3.1 to remove your dependency on legacy decorators.`
     );
@@ -66,7 +110,11 @@ function checkAddonsForStage1(project, addonOrProject) {
     return true;
   }
 
-  return addonOrProject.addons.some(addon => checkAddonsForStage1(project, addon));
+  if (addonOrProject.addons) {
+    return addonOrProject.addons.some(addon => checkAddonsForStage1(project, addon));
+  }
+
+  return false;
 }
 
 function setupBabelPlugins(project, addon, options) {
