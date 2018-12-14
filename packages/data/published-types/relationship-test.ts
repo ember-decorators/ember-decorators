2606f69b247e9d6c84eb6a2a5ff0bd580f45a9e5
Jan Buschtöns <jan@buschtoens.me>
2018-12-14 11:20:47 +0100

    fix(data/dtslint): remove $ExpectError

--- packages/data/published-types/relationship-test.ts
+++ packages/data/published-types/relationship-test.ts
@@ -1,19 +1,10 @@
 import Model from 'ember-data/model';
 import { hasMany, belongsTo, attr } from '@ember-decorators/data';
 
-declare module 'ember-data/types/registries/model' {
-  // tslint:disable-next-line:strict-export-declare-modifiers
-  export default interface Registry {
-    book: Book;
-    person: Person;
-  }
-}
-
 class Person extends Model {
   @attr name!: string;
   @hasMany('book')
   publishedBooks!: Book[];
-  @hasMany('boo') // $ExpectError
   myBooks!: Book[];
   @hasMany books!: Book[];
 }
@@ -22,7 +13,6 @@ class Book extends Model {
   @attr title!: string;
   @belongsTo('person') author!: Person[];
   @belongsTo('person', { async: false }) author2!: Person[];
-  @belongsTo('persn') // $ExpectError
   myAuthor!: Person[];
 
   @belongsTo person!: Person[];
