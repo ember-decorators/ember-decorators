8cc1849cdaca6464778bfdb2a17210b2a29c91b6
Chris Garrett <me@pzuraq.com>
2019-01-10 08:25:34 -0800

    [BUGFIX] Move all types to proper location in the root (#373)
    The recent change to move types into the `/published-types` directory and use the `"types"` field of `package.json` breaks down for `@ember-decorators/object` which has more than one module. [Limitations in the way that package.json's `main` and `types` fields](https://github.com/Microsoft/TypeScript/issues/8305) work prevent us from being able to point to a root directory, we can only point to one file for the index, so this strategy doesn't work for us.
    
    This PR switches us back to using `prepack` and `postpack` steps to move the typings to the correct location. This is the same direction that Embroider is moving too, so I'm pretty comfortable with doing this until that effort has been completed and we can rely on more standardized pre-publish tooling.
    
    Fixes #372, cc @buschtoens @mike-north

--- packages/-ember-decorators/tests/dummy/app/app.js
+++ packages/-ember-decorators/tests/dummy/app/app.js
@@ -1,7 +1,16 @@
+/* globals define */
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import EmberRouter from '@ember/routing/router';
+
+// Including ember-cli-addon-docs is having issues with earlier versions of Node
+// due to dependencies that require Node v8, this is a temporary measure so we
+// don't have to drop Node v6 support
+define('ember-cli-addon-docs/router', () => {
+  return EmberRouter;
+});
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
