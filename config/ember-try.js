008ebf2249750b2b50b03d27f5f5c8abf35ed6b4
Chris Manson <chris@manson.ie>
2026-05-28 10:20:29 +0100

    drop support for Ember < 3.8

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,30 +13,6 @@ module.exports = function() {
       scenarios: [
         // Ember Versions
         {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-3.4',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.4.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.8',
           npm: {
             devDependencies: {
